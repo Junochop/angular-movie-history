5077968345a5cedb0f96bb7c25a212bd73ce54d0
Juno Lee <lejuno80@gmail.com>
2017-11-15 20:06:04 -0600

    movie rating

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -1,5 +1,12 @@
 'use strict';
 
-app.controller("RatedCtrl", function($scope){
-	$scope.controller = "RatedCtrl";
+app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
+	$scope.movies =[];
+
+	MovieService.getRatedMovies($rootScope.uid).then((results) => { //load on page loade 
+		$scope.movies = results;
+	}).catch((err) => {
+		console.log("error in the get rated", err);
+	});
+
 });
